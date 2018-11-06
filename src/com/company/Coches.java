44cbe16d30f5873d300ab9ea8dc1c9f27521889a
amateu <amateu@elpuig.xeill.net>
2018-11-06 17:52:59 +0100

    Basic1

--- src/com/company/Coches.java
+++ src/com/company/Coches.java
@@ -6,16 +6,21 @@ public class Coches {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        //pregunta el numero de coches
         System.out.println("Cuantos coches hay?");
         int[] gap;
         int total = 0;
         int coches = teclado.nextInt();
+        //crea el array con el numero de coches
         gap = new int[coches];
+        //entra en for hasta que i sea menor a la longitud del array y pide los tiempos entre los coches
+        //suma todos los valores del array
         for (int i = 0; i < gap.length; i++) {
             System.out.println("Introduce tiempos entre los coches " + i);
             gap[i] = teclado.nextInt();
             total+=gap[i];
         }
-        System.out.println("Entre el primer coche y el último hay " + total + " segundos.");
+        //lo saca por pantalla
+        System.out.println("Entre el primer y último coche hay " + total + " segundos.");
     }
 }
