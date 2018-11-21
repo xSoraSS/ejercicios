8e44cf06e3a03abbba589c6a9478b60ae74e3f7c
amateu <amateu@elpuig.xeill.net>
2018-11-21 18:46:51 +0100

    Triangulo

---
+++ src/com/company/T1.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class T1 {
+
+    public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+        int num = teclado.nextInt(), n1 = num;
+        while (num != 0) {
+            int n2 = num;
+            num = teclado.nextInt();
+                if (n1 == n2 && num == 0) {
+                    System.out.println("SI");
+                    break;
+                } else if (n1 != n2 && num == 0) {
+                    System.out.println("NO");
+                    break;
+            }
+        }
+    }
+}
